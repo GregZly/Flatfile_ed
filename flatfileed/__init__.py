ed7da78d1a1e2a198fc41f87d8edbeeb72d51403
Grzegorz Grzegorczyk <grzesiek.grzegorczyk@gmail.com>
2019-06-05 07:00:22 +0200

    init.py created from flask tutorial

---
+++ flatfileed/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    #create and configure application
+    app = Flask(__nam__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev'
+    )
+
+    if test_config is None:
+        #Load instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        #Load the test config if passed in
+        app.config.from_mapping(test_config)
+    
+    #ensure the instantce folder exist
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    #hello page for test
+    @app.route('/hello')
+    def hello():
+        return "Hello World"
+    
+    return app
